9e241f95ff759e35f2cbad568c4f5481dbb0acd0
veloya <archaic.archea@gmail.com>
2023-04-30 12:13:41 -0400

    Uhhhhh, UB, as soon as its observed, it goes away :uhhh:

--- LSD/src/memory/vmm.rs
+++ LSD/src/memory/vmm.rs
@@ -80,7 +80,7 @@ impl<'a, 'b> Vmm <'a, 'b> {
 
             let phys = unmap(current_table().cast_mut(), virt, level, PageLevel::Level1).0 + super::HHDM_OFFSET.load(Ordering::Relaxed);
 
-            super::pmm::REGION_LIST.lock().shove(phys as *mut u8);
+            //super::pmm::REGION_LIST.lock().shove(phys as *mut u8);
 
             flush_tlb(Some(virt), None);
         }
@@ -254,6 +254,8 @@ pub unsafe fn unmap(
             let return_addr = entry.get_ppn() << 12;
             entry.0 = 0;
 
+            println!("Old table dump: \n{:?}", table.read_volatile());
+            println!("New table dump: \n{:?}", table_copy);
             table.write_volatile(table_copy);
 
             return PhysicalAddress(return_addr);
