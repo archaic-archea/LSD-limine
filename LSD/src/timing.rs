ff695002fbee9e80e74ae41eb98b25692071a85f
veloya <archaic.archea@gmail.com>
2023-05-03 10:40:02 -0400

    Even more units

--- LSD/src/timing.rs
+++ LSD/src/timing.rs
@@ -6,8 +6,12 @@ pub enum Unit {
     Weeks(u64),
     Days(u64),
     Hours(u64),
+    Hectoseconds(u64),
     Minutes(u64),
+    Decaseconds(u64),
     Seconds(u64),
+    Deciseconds(u64),
+    Centiseconds(u64),
     MilliSeconds(u64),
     MicroSeconds(u64),
 }
@@ -17,31 +21,17 @@ impl Unit {
         let timer_speed = TIMER_SPEED.load(Ordering::Relaxed);
 
         match self {
-            Unit::Weeks(wks) => {
-                timer_speed * wks * 604800
-            },
-            Unit::Days(days) => {
-                timer_speed * days * 86400
-            },
-            Unit::Hours(hrs) => {
-                timer_speed * hrs * 3600
-            },
-            Unit::Minutes(mins) => {
-                timer_speed * mins * 60
-            },
-            Unit::Seconds(secs) => {
-                timer_speed * secs
-            },
-            Unit::MilliSeconds(ms) => {
-                let ms = ms / 1000;
-
-                timer_speed * ms
-            },
-            Unit::MicroSeconds(us) => {
-                let us = us / 1000000;
-
-                timer_speed * us
-            }
+            Unit::Weeks(wks) =>         {timer_speed * wks * 604800},
+            Unit::Days(days) =>         {timer_speed * days * 86400},
+            Unit::Hours(hrs) =>         {timer_speed * hrs * 3600},
+            Unit::Hectoseconds(hs) =>   {timer_speed * hs * 100},
+            Unit::Minutes(mins) =>      {timer_speed * mins * 60},
+            Unit::Decaseconds(ds) =>    {timer_speed * ds * 10},
+            Unit::Seconds(secs) =>      {timer_speed * secs},
+            Unit::Deciseconds(dis) =>   {timer_speed * dis / 10},
+            Unit::Centiseconds(cs) =>   {timer_speed * cs / 100},
+            Unit::MilliSeconds(ms) =>   {timer_speed * ms / 1000},
+            Unit::MicroSeconds(us) =>   {timer_speed * us / 1000000}
         }
     }
 
